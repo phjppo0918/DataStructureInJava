1298a6bca1d9e91efa6cf7ada91161a9e47b196a
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-10-15 14:30:04 +0000

    10-1 ing

---
+++ CHAP10/Example1.java
@@ -0,0 +1,44 @@
+class AdjMatrix{
+  private int matrix [][] = new int [10][10];
+  private int totalV = 0;
+
+  public void insertVertex(int v){
+    totalV++;
+  }
+
+  public void insertEdge(int vi, int v2){
+    if(v1 >= totalV || v2 >=totalv){
+      System.out.println("그래프에 없는 정점입니다!!");
+    }else{
+      matrix[v][z];
+    }
+  }
+
+  public void printMatrix(){
+    for(int i=0;i<totalV; i++) {
+      System.out.println("\n\t\t");
+      for(int j=0; j<totalV; j++){
+        System.out.print(matrix[i][j]);
+      }
+    }
+  }
+
+}
+class GraphNode{
+  int vertex;
+  GraphNode link;
+}
+
+class AdjList {
+  private GraphNode head[] = new GraphNode[10];
+  private int totalV = 0;
+
+  public voidinsertVertex(int v){
+    totalV++;
+  }
+
+  public void insertEdge(int v1, int v2) {
+    if(v1=totalV)|| (v2>= total)System == new Greach
+  }
+  
+  }
