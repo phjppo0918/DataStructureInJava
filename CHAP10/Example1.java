2f497a92a5f7e4bf877d348f7ac62f90b964b9cb
Hyeonjun Park <phjppo0918@kpu.ac.kr>
2020-10-16 14:04:41 +0000

    10-1 fin

--- CHAP10/Example1.java
+++ CHAP10/Example1.java
@@ -6,11 +6,11 @@ class AdjMatrix{
     totalV++;
   }
 
-  public void insertEdge(int vi, int v2){
-    if(v1 >= totalV || v2 >=totalv){
+  public void insertEdge(int v1, int v2){
+    if(v1 >= totalV || v2 >=totalV){
       System.out.println("그래프에 없는 정점입니다!!");
     }else{
-      matrix[v][z];
+      matrix[v1][v2] = 1;
     }
   }
 
@@ -18,7 +18,7 @@ class AdjMatrix{
     for(int i=0;i<totalV; i++) {
       System.out.println("\n\t\t");
       for(int j=0; j<totalV; j++){
-        System.out.print(matrix[i][j]);
+        System.out.print(matrix[i][j]+ " ");
       }
     }
   }
@@ -33,12 +33,71 @@ class AdjList {
   private GraphNode head[] = new GraphNode[10];
   private int totalV = 0;
 
-  public voidinsertVertex(int v){
+  public void insertVertex(int v){
     totalV++;
   }
 
   public void insertEdge(int v1, int v2) {
-    if(v1=totalV)|| (v2>= total)System == new Greach
+    if((v1>=totalV)||(v2>=totalV)){
+      System.out.println("그래프에 없는 정점입니다!");
+    }else{
+      GraphNode gNode = new GraphNode();
+      gNode.vertex = v2;
+      gNode.link = head[v1];
+      head[v1] = gNode;
+    }
+  }
+
+  public void printAdjList(){
+    GraphNode gNode = new GraphNode();
+    for(int i=0;i<totalV; i++){
+      System.out.printf("\n정점 %c의 연결 리스트", i+65);
+      gNode = head[i];
+      while(gNode != null) {
+        System.out.printf("-> %c ", gNode.vertex+65);
+        gNode = gNode.link;
+      }
+    }
   }
   
+}
+
+class Example1 {
+  public static void main(String [] args) {
+    AdjMatrix MG1 = new AdjMatrix();
+    for(int i=0;i<4;i++){
+      MG1.insertVertex(i);
+    }
+    MG1.insertEdge(0,3);
+    MG1.insertEdge(0,1);
+    MG1.insertEdge(1,3);
+    MG1.insertEdge(1,2);
+    MG1.insertEdge(1,0);
+    MG1.insertEdge(2,3);
+    MG1.insertEdge(2,1);
+    MG1.insertEdge(3,2);
+    MG1.insertEdge(3,1);
+    MG1.insertEdge(3,0);
+    System.out.print("\n 그래프 G1의 인접 행렬 :");
+    MG1.printMatrix();
+    System.out.println();
+
+    AdjList LG1 = new AdjList();
+    for(int i=0; i<4; i++){
+      LG1.insertVertex(i);
+    }
+    LG1.insertEdge(0,3);
+    LG1.insertEdge(0,1);
+    LG1.insertEdge(1,3);
+    LG1.insertEdge(1,2);
+    LG1.insertEdge(1,0);
+    LG1.insertEdge(2,3);
+    LG1.insertEdge(2,1);
+    LG1.insertEdge(3,2);
+    LG1.insertEdge(3,1);
+    LG1.insertEdge(3,0);
+    System.out.print("\n 그래프 G1의 인접 리스트 :");
+    LG1.printAdjList();
+    System.out.println();
   }
+}
